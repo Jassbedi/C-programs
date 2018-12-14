0864b2f7b4bf2eeffd49443a85d3fa699531f2a1
jaspreet singh <jaspreetsingh@jaspreets-MacBook-Air.local>
2018-12-14 13:20:43 +0530

    vowel,ascii,reverse

---
+++ reversestring.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+//#include <string.h>
+int  main(){
+  char str[15],strrev;
+
+  printf("Enter string: ");
+  gets(str);
+  printf("String is: %s",str);
+  strrev(str);
+  printf("\nReverse String is: %s",str;
+ return 0;
+}
