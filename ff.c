464b85d9161873358a6da288830c12cbe864eb1a
Jaspreet singh bedi <ekqumjit14837@gmail.com>
2018-12-20 23:55:36 +0530

    Create ff.c
    kaintfile

---
+++ ff.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+int main()
+{
+int i=0,no;
+printf("enter the integer value=");
+scanf("%d",&no);
+
+if(no>0)
+
+
+{
+  for(i=no;i<=no;--i)
+i=no*no*1;
+printf("integer=%d\n",i);
+}
+
+
+
+
+
+return 0;
+}
